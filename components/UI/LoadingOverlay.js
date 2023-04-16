381ee825c98f48947c483d1df94d861952a9ce41
headhunter225014 <105608219+headhunter225014@users.noreply.github.com>
2023-04-15 20:39:37 -0400

    ofek

--- components/UI/LoadingOverlay.js
+++ components/UI/LoadingOverlay.js
@@ -18,7 +18,7 @@ const styles =StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        padding: 24,
+        padding: 20,
         backgroundColor: GlobalStyles.colors.primary700
     }
 });
