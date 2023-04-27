654699e2048f413b956354dc1e4b9f95f736568f
headhunter225014 <105608219+headhunter225014@users.noreply.github.com>
2023-04-26 20:56:27 -0400

    style

--- components/UI/LoadingOverlay.js
+++ components/UI/LoadingOverlay.js
@@ -18,7 +18,7 @@ const styles =StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        padding: 15,
+        padding: 16,
         backgroundColor: GlobalStyles.colors.primary700
     }
 });
